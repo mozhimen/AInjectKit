b6241d35a5738910371111f375bbb27d1968625c
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-06-17 22:19:24 +0800

    0617

---
+++ injectk_hilt_startup/src/main/java/com/mozhimen/injectk/hilt/startup/bases/BaseHiltInitializer.kt
@@ -0,0 +1,17 @@
+package com.mozhimen.injectk.hilt.startup.bases
+
+import androidx.startup.Initializer
+
+/**
+ * @ClassName BaseHiltInitializer
+ * @Description TODO
+ * @Author Mozhimen & Kolin Zhao
+ * @Date 2024/6/17
+ * @Version 1.0
+ */
+abstract class BaseHiltInitializer : Initializer<Unit> {
+    abstract fun getDependencyGraphInitializer(): Class<out BaseDependencyGraphInitializer>
+    override fun dependencies(): List<Class<out Initializer<*>>> {
+        return listOf(getDependencyGraphInitializer())
+    }
+}
